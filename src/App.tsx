142ef1fb35be8d0df4ba9bb6028992f9e69de790
Stanley Osei-Wusu <107648937+stanleymay20@users.noreply.github.com>
2026-09-21 08:55:39 +0200

    Declare admin pages as relative nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,15 @@ const VideosPage = lazy(() => import("./pages/VideosPage"));
 const CoursesPage = lazy(() => import("./pages/CoursesPage"));
 const BooksPage = lazy(() => import("./pages/BooksPage"));
 const AdminApp = lazy(() => import("./components/AdminApp"));
+const AdminIndex = lazy(() => import("./pages/admin/index"));
+const AdminProfilePage = lazy(() => import("./pages/admin/ProfilePage"));
+const AdminProjectsPage = lazy(() => import("./pages/admin/ProjectsPage"));
+const AdminBooksPage = lazy(() => import("./pages/admin/BooksPage"));
+const AdminVideosPage = lazy(() => import("./pages/admin/VideosPage"));
+const AdminMediaPage = lazy(() => import("./pages/admin/MediaPage"));
+const AdminVersesPage = lazy(() => import("./pages/admin/VersesPage"));
+const AdminCoursesPage = lazy(() => import("./pages/admin/CoursesPage"));
+const SiteSettingsPage = lazy(() => import("./pages/admin/SiteSettingsPage"));
 
 const RouteFallback = () => (
   <div className="min-h-screen bg-background flex items-center justify-center">
@@ -29,7 +38,19 @@ const App = () => (
         <Route path="/videos" element={<VideosPage />} />
         <Route path="/courses" element={<CoursesPage />} />
         <Route path="/books" element={<BooksPage />} />
-        <Route path="/admin/*" element={<AdminApp />} />
+
+        <Route path="/admin" element={<AdminApp />}>
+          <Route index element={<AdminIndex />} />
+          <Route path="profile" element={<AdminProfilePage />} />
+          <Route path="projects" element={<AdminProjectsPage />} />
+          <Route path="books" element={<AdminBooksPage />} />
+          <Route path="videos" element={<AdminVideosPage />} />
+          <Route path="media" element={<AdminMediaPage />} />
+          <Route path="verses" element={<AdminVersesPage />} />
+          <Route path="courses" element={<AdminCoursesPage />} />
+          <Route path="settings" element={<SiteSettingsPage />} />
+        </Route>
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Suspense>
